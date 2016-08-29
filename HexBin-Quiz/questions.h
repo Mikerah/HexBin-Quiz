5e6fa2a4ddfb76a6e5d1cbe3623d3a928267f6c5
Mikerah <mikerah14@gmail.com>
2016-08-29 13:38:44 -0400

    added the correct functions definitions to questions.h

--- HexBin-Quiz/questions.h
+++ HexBin-Quiz/questions.h
@@ -1,7 +1,6 @@
 #ifndef QUESTIONS_H
 #define QUESTIONS_ H
 
-// Saves a quiz question to a file
-void save_question_to_file(FILE * fp, char * question);
+char * generate_questions(int number_of_questions, char * question_code);
 
 #endif // QUESTIONS_H
