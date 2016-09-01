b90b8237441d1ccae6799df9a9a105669a4c4327
Mikerah <mikerah14@gmail.com>
2016-08-31 21:07:54 -0400

    completed functions definitions in numbers.c and changed function prototypes in numbers.h

--- HexBin-Quiz/numbers.c
+++ HexBin-Quiz/numbers.c
@@ -1,1 +1,68 @@
+#include "numbers.h"
+#include <time.h>
+#include <stdlib.h>
+#include <math.h>
+
+char * convert_dec_to_bin(char * to_convert) {
+    int num_in_bin = strtol(to_convert, NULL, 2);
+    char num = num_in_bin + '0';
+    return num;
+
+}
+
+char * convert_dec_to_hex(char * to_convert) {
+    char hex[50];
+    sprintf(&hex, "%x", input);
+    return hex;
+}
+
+char * convert_bin_to_dec(char * to_convert) {
+    int num_in_dec = strtol(to_convert, NULL, 10);
+    char num = num_in_dec + '0';
+    return num;
+}
+
+// Convert a number from binary to hexidecimal
+char * convert_bin_to_hex(char * to_convert) {
+    int num = strtol(to_convert, NULL, 2);
+    char hex[50];
+    sprintf(&hex, "%x", num);
+    return hex;
+}
+
+// Convert a number from hexidecimal to decimal
+char * convert_hex_to_dec(char * to_convert) {
+    char hex[50];
+    sprintf(&hex, "%x", to_convert);
+    int num_in_dec = strtol(hex, NULL, 10);
+    char num = num_in_dec + '0';
+    return num;
+
+}
+
+// Convert a number from hexidecimal to binary
+char * convert_hex_to_bin(char * to_convert) {
+    char hex[50];
+    sprintf(&hex, "%x", to_convert);
+    int num_in_bin = strtol(hex, NULL, 2);
+    char num = num_in_bin + '0';
+    return num;
+}
+
+char * generate_decimal_number() {
+    srand(time(NULL));
+    return rand() + '0';
+}
+
+// Generates a random hexicidemal number
+char * generate_hexadecimal_number() {
+    srand(time(NULL));
+    return convert_dec_to_hex(rand() + '0');
+}
+
+// Generates a random binary number
+char * generate_binary_number() {
+    srand(time(NULL));
+    return convert_dec_to_bin(rand() + '0');
+}
 
--- HexBin-Quiz/numbers.h
+++ HexBin-Quiz/numbers.h
@@ -1,31 +1,32 @@
 #ifndef NUMBERS_H
 #define NUMBERS_H
 
-// Generates a randomm decimal number
-int generate_decimal_number();
-
-// Generates a random hexicidemal number
-int generate_hexidecimal_number();
-
-// Generates a random binary number
-int generate_binary_number();
-
 // Converts a number from decimal to binary
-int convert_dec_to_bin(int to_convert);
+char * convert_dec_to_bin(char * to_convert);
 
 // Converts a number from decimal to hexidecimal
-int convert_dec_to_hex(int to_convert);
+char * convert_dec_to_hex(char * to_convert);
 
 // Convert a number from binary to decimal
-int convert_bin_to_dec(int to_convert);
+char * convert_bin_to_dec(char * to_convert);
 
+char * convert_dec_to_hex(char * to_convert);
 // Convert a number from binary to hexidecimal
-int convert_bin_to_hex(int to_convert);
+char * convert_bin_to_hex(char * to_convert);
 
 // Convert a number from hexidecimal to decimal
-int convert_hex_to_dec(int to_convert);
+char * convert_hex_to_dec(char * to_convert);
 
 // Convert a number from hexidecimal to binary
-int convert_hex_to_bin(int to_convert);
+char * convert_hex_to_bin(char * to_convert);
+
+// Generates a randomm decimal number
+char * generate_decimal_number();
+
+// Generates a random hexicidemal number
+char * generate_hexadecimal_number();
+
+// Generates a random binary number
+char * generate_binary_number();
 
 #endif // NUMBERS_H
