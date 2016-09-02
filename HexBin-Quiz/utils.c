e714f62f9c9ff77b362900eccefabcb20485488d
Mikerah <mikerah14@gmail.com>
2016-09-01 21:04:18 -0400

    completed function in utils.c

--- HexBin-Quiz/utils.c
+++ HexBin-Quiz/utils.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.c"
 
+
+void save_question_to_file(FILE * fp, char * question, char * file_name) {
+    fp = fopen(file_name, "w+");
+    fprintf(fp, "%s\n", question);
+    fclose(fp);
+}
